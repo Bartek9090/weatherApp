7da9e0a42a236ea1b23d516d5ca2450f2ba4054c
Bartek <b.bartek987@gmail.com>
2021-09-30 20:15:05 +0100

    add display temp by lcation

--- asset/js/app.js
+++ asset/js/app.js
@@ -13,3 +13,20 @@ switcher.addEventListener('click', () => {
 
     }
 })
+
+let APIURL = "http://api.weatherapi.com/v1/current.json?key=21d65c223c50438b841180651213009%20&q="
+let weather_form = document.querySelector('.weather-form');
+let data_container = document.querySelector('.data-from-api')
+weather_form.addEventListener('submit', (event) => {
+    let city = document.querySelector('.city').value;
+    let NEWAPIURL = APIURL + city;
+    fetch(NEWAPIURL)
+        .then(response => response.json())
+        .then((data) => {
+            data_container.innerHTML += `<div class='main-info'>Today (${data.location.localtime}) 
+                 in ${data.location.name} is ${data.current.temp_c}<sup>o</sup>C </div>`
+        })
+
+
+    event.preventDefault();
+})
